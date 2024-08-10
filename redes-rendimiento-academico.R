889393526503ae484aa7d0f904776b51a4489a57
vcardonas <vcardonas@unal.edu.co>
2024-08-10 16:13:53 -0500

    updt1

--- redes-rendimiento-academico.R
+++ redes-rendimiento-academico.R
@@ -10,7 +10,7 @@ rm(list = ls())
 #===================#
 
 # Lista de paquetes que se necesitan
-paquetes <- c("tidyr", "dplyr", "tidyverse", "magrittr", "igraph")
+paquetes <- c("readxl", "tidyr", "dplyr", "tidyverse", "magrittr", "igraph", "qgraph")
 # Utilizar lapply para cargar cada paquete si aún no está instalado
 paquetes_cargar <- lapply(paquetes, 
                           FUN = function(x){
@@ -39,43 +39,121 @@ list.files("Datos")
 # Importar RData
 load("Datos/Datos_COL.RData")
 
-# Encontrar la posición de la columna por el nombre
-which(names(STU_QQQ_COL) == "MATH")
-
-unique(STU_QQQ_COL$ICTRES)
-
-STU_QQQ_COL
-
-STU_QQQ_COL %<>% 
+# Calcular media de las áreas
+datos <- STU_QQQ_COL %>% 
+  left_join(CRT_COG_COL[c("CNTSTUID", paste0("PV", 1:10, "CRTH_NC"))], by = "CNTSTUID") %>% 
   mutate(MATH = rowMeans(across(PV1MATH:PV10MATH), na.rm = TRUE),
          READ = rowMeans(across(PV1READ:PV10READ), na.rm = TRUE),
-         SCIE = rowMeans(across(PV1SCIE:PV10SCIE), na.rm = TRUE))
+         SCIE = rowMeans(across(PV1SCIE:PV10SCIE), na.rm = TRUE),
+         CRTH = rowMeans(across(PV1CRTH_NC:PV10CRTH_NC), na.rm = TRUE))
 
-Data <- STU_QQQ_COL[,c(4,7,9,26,967,970,975,976,977,981,982,983,984,985,986,
-                       # WLE
-                       990:1029, 1037:1083, 
-                       # Escalas PISA
-                       1279:ncol(STU_QQQ_COL))]
-Data %<>%
-  mutate(across(everything(), ~ as.numeric(as.character(.)), .names = "{col}")) %>% 
-  dplyr::select(where(~ !all(is.na(.))))
+# Seleccionar variables de estudio
+datos %<>%
+  dplyr::select(
+    # Identificados
+    CNTSTUID,
+    # Escalas
+    MATH, READ, SCIE, CRTH,
+    # Factores demográficos
+    ST004D01T, STRATUM, REGION, IMMIG,
+    # Factores socioeconómicos
+    MISCED, FISCED, ICTRES, HOMEPOS, ESCS,
+    # Factores educativos
+    RELATST, BELONG, BULLIED, FEELSAFE, SCHRISK,
+    SCHSUST, LEARRES, PROBSELF, PARINVOL, PQSCHOOL,
+    # Factores psicológicos
+    PERSEVAGR, CURIOAGR, COOPAGR, EMOCOAGR, GROSAGR,
+    # Factores contextuales
+    WORKPAY, WORKHOME, FAMSUP, 
+    # Factores de aprendizaje
+    STUDYHMW, REPEAT, MISSSC, SKIPPING, FAMSUPSL,
+    # Matemáticas
+    MATHPREF, MATHEASE, MATHMOT, DISCLIM, TEACHSUP,
+    FAMCON, ANXMAT, MATHPERS,
+    # Pensamiento creativo
+    CREATEFF, CREATSCH, CREATFAM, CREATAS, CREATOOS, CREATOP, 
+    OPENART, IMAGINE,
+    CREATHME, CREATACT, CREATOPN, CREATOR
+    )
 
-# Crear la matriz de correlación y tomar los valores absolutos
-cor_matrix <- cor(Data, use = "complete.obs") %>%
-  as.data.frame() %>% 
-  mutate(across(everything(), abs))
-cor_matrix <- as.matrix(cor_matrix)
+# Importar CodeBook
+list.files("Datos")
+codebook <- read_excel("Datos/CY08MSP_CODEBOOK_27thJune24.xlsx", sheet = "CY08MSP_STU_QQQ")
+labels <- codebook %>%
+  fill(NAME, VARLABEL, TYPE, FORMAT, VARNUM, MINMAX, .direction = "down") %>% 
+  drop_na(VAL)
+
+# Usar codificaciones numéricas
+for (col in names(datos)) {
+  if (col %in% labels$NAME) {
+    label_map <- labels %>%
+      filter(NAME == col) %>%
+      select(LABEL, VAL) %>%
+      deframe()
+    datos[[col]] <- recode(datos[[col]], !!!label_map)
+  }
+}
+
+# Convertir todas las columnas a numéricas
+datos %<>% mutate_all(as.numeric)
+
+# Verificar
+## Si hay columnas con valores nulos > 50% de los datos
+tmp <- colSums(is.na(datos)) / nrow(datos) * 100
+names(tmp[tmp > 50])
+
+#=========================#
+#### 4. Preparar grafo ####
+#=========================#
+
+# Crear la matriz de correlaciones parciales
+cor_matrix <- cor_auto(datos[-1], forcePD = TRUE)
 
 # Reemplazar los valores en la matriz con 0 en la diagonal
 diag(cor_matrix) <- 0
 
-# Crear una matriz de pesos con 0 si la correlación es menor o igual a 0.3
-weights_matrix <- ifelse(cor_matrix > 0.3, cor_matrix, 0)
-
 # Crear el grafo a partir de la matriz de adyacencia con pesos
-g <- graph_from_adjacency_matrix(weights_matrix, mode = "undirected", weighted = TRUE)
+g <- graph_from_adjacency_matrix(cor_matrix, mode = "undirected", weighted = TRUE)
+graph <- qgraph(cor_matrix,
+                graph = "glasso",
+                sampleSize = nrow(datos),
+                layout = "spring")
 
-# Plotear el grafo
+#================================#
+#### 5. Análisis exploratorio ####
+#================================#
+
+# Visualización 
 plot(g, edge.width = E(g)$weight * 5)
+
+centralityPlot(g)
+
+qgraph(cor_matrix, title = "Correlation Graph", labels = colnames(cor_matrix))
+qgraph(cor_matrix, graph = "pcor", layout = "spring", cut = 0)
+
+VSS.scree(cor_matrix)
+
+getWmat(graph)
+centralityPlot(graph)
+
+#================================#
+#### 6. Modelo ####
+#================================#
+
+#=============================#
+#### 7. Validación cruzada ####
+#=============================#
+
+
+
+
+
+
+
+
+
+
+
+
 
 
